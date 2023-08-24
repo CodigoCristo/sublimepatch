3856513abdb028bc9dd9bba7c11ca7ef5f3b789f
Codigo Cristo <cristophero.alvarado@gmail.com>
2023-08-24 14:33:17 -0500

    update
    update version

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ void pause();
 void clean();
 void msg_menu ();
 void msg_end ();
-void patch_sublime4143 ();
+void patch_sublime ();
 void patch_sublimeDEV ();
-void patch_merge2079 ();
+void patch_merge ();
 
 
 int main ()
@@ -26,24 +26,24 @@ int main ()
 	int optionsublime = 0 ;
 	int optionmerge = 0 ;
 
-	printf("\t1. Sublime Text Stable 4143\n");
-	printf("\t2. Sublime Text DEV 4147\n");
-	printf("\t3. Sublime Merge Stable 2079\n");
+	printf("\t1. Sublime Text Linux Build 4152 (Stable)\n");
+	printf("\t2. Sublime Text Linux Build 4154 (Dev Channel)\n");
+	printf("\t3. Sublime Merge Stable 2090\n");
 	printf("\nEnter the number (1 - 2 - 3) : ");
 	scanf ("%i", &option);
 
 	switch( option )
     {
         case 1:
-            patch_sublime4143 ();
+            patch_sublime ();
             break;
 
         case 2:
-	    patch_sublimeDEV ();
+	        patch_sublimeDEV ();
             break;
             
         case 3:
-	    patch_merge2079 ();
+	        patch_merge ();
             break;
 
         default:
@@ -74,28 +74,31 @@ void clean()
 
 
 
-void patch_sublime4143 () 
+void patch_sublime () 
 {
 
     system("cp /opt/sublime_text/sublime_text '.' ");
 
     FILE *crack = fopen( "sublime_text", "r+b" );
-    fseek( crack, 0x003A31F2, SEEK_SET ); 
+    fseek( crack, 0x00415013, SEEK_SET ); 
     fputs("\x48\x31\xC0\xC3", crack);
 
-    fseek( crack, 0x00399387, SEEK_SET ); 
+    fseek( crack, 0x00409037, SEEK_SET ); 
     fputs("\x90\x90\x90\x90\x90", crack);
 
-    fseek( crack, 0x0039939D, SEEK_SET ); 
+    fseek( crack, 0x0040904F, SEEK_SET ); 
     fputs("\x90\x90\x90\x90\x90", crack);
 
-    fseek( crack, 0x003A4E30, SEEK_SET ); 
+    fseek( crack, 0x00416CA4, SEEK_SET ); 
     fputs("\x48\x31\xC0\x48\xFF\xC0\xC3", crack);
 
-    fseek( crack, 0x003A2E82, SEEK_SET ); 
+    fseek( crack, 0x00416CA4, SEEK_SET ); 
     fputs("\xC3", crack);
 
-    fseek( crack, 0x0038C9F0, SEEK_SET ); 
+    fseek( crack, 0x00414C82, SEEK_SET ); 
+    fputs("\xC3", crack);
+
+    fseek( crack, 0x003FA310, SEEK_SET ); 
     fputs("\xC3", crack);
     fclose( crack );
     
@@ -130,16 +133,16 @@ void patch_sublimeDEV ()
     system("cp /opt/sublime_text/sublime_text '.' ");
 
     FILE *crack = fopen( "sublime_text", "r+b" );
-    fseek( crack, 0x413CCF, SEEK_SET ); 
+    fseek( crack, 0x443F94, SEEK_SET ); 
     fputs("\x48\x31\xC0\xC3", crack);
     
-    fseek( crack, 0x407CD9, SEEK_SET ); 
+    fseek( crack, 0x42B210, SEEK_SET ); 
     fputs("\x90\x90\x90\x90\x90", crack);
 
-    fseek( crack, 0x407CF1, SEEK_SET ); 
+    fseek( crack, 0x42B228, SEEK_SET ); 
     fputs("\x90\x90\x90\x90\x90", crack);
 
-    fseek( crack, 0x41594C, SEEK_SET ); 
+    fseek( crack, 0x445EB6, SEEK_SET ); 
     fputs("\x48\x31\xC0\x48\xFF\xC0\xC3", crack);
 
     fseek( crack, 0x432C88, SEEK_SET ); 
@@ -174,36 +177,84 @@ void patch_sublimeDEV ()
 }
 
 
-void patch_merge2079 () 
+/*
+void patch_merge () 
 {
 
     system("cp /opt/sublime_merge/sublime_merge '.' ");
 
-    char b1[] = "\x48\x31\xC0\xC3";
+    FILE *crack = fopen( "sublime_merge", "r+b" );
+    fseek( crack, 0x0045A360, SEEK_SET ); 
+    fputs("\x48\xC7\xC0\x19\x01\x00\x00\xC3", crack);
+    
+    fseek( crack, 0x0045D21D, SEEK_SET ); 
+    fputs("\x90\x90\x90\x90\x90", crack);
+
+    fseek( crack, 0x0045D23A, SEEK_SET ); 
+    fputs("\x90\x90\x90\x90\x90", crack);
+
+    fseek( crack, 0x0045B990, SEEK_SET ); 
+    fputs("\xC3", crack);
+
+    fseek( crack, 0x0045A05A, SEEK_SET ); 
+    fputs("\xC3", crack);
+
+    fseek( crack, 0x00459ABA, SEEK_SET ); 
+    fputs("\xC3", crack);
+    fclose( crack );
+    
+    system("mv sublime_merge /opt/sublime_merge/sublime_merge ");
+    
+    system("clear");
+    msg_menu ();
+    printf("\n");
+    printf("\tSublime Text for Linux x64 platform, loading...\n");
+    system("echo '\nPress enter to continue...' &&  read line");
+
+    system("clear");
+    msg_menu ();
+    printf("\n");
+    printf("\tPaying $99 USD For A License Is Stupid.\n");
+    system("echo '\nPress enter to continue...' &&  read line");
+
+    system("clear");
+    msg_menu ();
+    printf("\n");
+    printf("\tEnter any key to program, program made by Codigo Cristo\n"); 
+    msg_end () ;
+    system("echo '\nPress enter to continue...' &&  read line");
+    system("clear; exit");
+
+}
+*/
+
+
+void patch_merge () 
+{
+
+    system("cp /opt/sublime_merge/sublime_merge '.' ");
+
+    char b1[] = "\x48\xC7\xC0\x19\x01\x00\x00\xC3";
     char b2[] = "\x90\x90\x90\x90\x90";
     char b3[] = "\x90\x90\x90\x90\x90";
     char b4[] = "\x48\x31\xC0\x48\xFF\xC0\xC3";
     char b5[] = "\xC3";
-    char b6[] = "\xC3";
 
     FILE *crack = fopen( "sublime_merge", "r+b" );
-    fseek( crack, 0x003CC9BA, SEEK_SET ); // Se mueve a la dirección o numero espacios desde el comienzo del archivo
+    fseek( crack, 0x00682CF3, SEEK_SET ); // Se mueve a la dirección o numero espacios desde el comienzo del archivo
     fwrite(b1, sizeof(b1[0]), 4, crack);
 
-    fseek( crack, 0x003CF9DD, SEEK_SET ); 
+    fseek( crack, 0x004E48C7, SEEK_SET ); 
     fwrite(b2, sizeof(b2[0]), 5, crack);
 
-    fseek( crack, 0x003CF9F3, SEEK_SET ); 
+    fseek( crack, 0x004E48DF, SEEK_SET ); 
     fwrite(b2, sizeof(b3[0]), 5, crack);
 
-    fseek( crack, 0x003CDFA2, SEEK_SET ); 
+    fseek( crack, 0x004E2FBC, SEEK_SET ); 
     fwrite(b3, sizeof(b4[0]), 7, crack);
 
-    fseek( crack, 0x003CC6D2, SEEK_SET ); 
+    fseek( crack, 0x004E141E, SEEK_SET ); 
     fwrite(b4, sizeof(b5[0]), 1, crack);
-
-    fseek( crack, 0x003CC130, SEEK_SET ); 
-    fwrite(b4, sizeof(b6[0]), 1, crack);
     fclose( crack );
     
     system("mv sublime_merge /opt/sublime_merge/sublime_merge ");
